43e4ab1cafd0d76b8a6ae685ddc76096d0565b57
pji0219 <pji219@naver.com>
2022-11-10 02:51:50 +0900

    상태 끌어 올리기

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './components/Counter';
 import './App.css';
 
 function AppCounter() {
+  const [totalCount, setTotalCount] = useState(0);
+
+  const totalCountHandler = () => {
+    setTotalCount((prev) => prev + 1);
+  };
+
   return (
     <div className='container'>
-      <Counter />
+      <div className='banner'>
+        Total Count: {totalCount} {totalCount > 10 ? '🔥' : '❄'}
+      </div>
+      <Counter totalCount={totalCount} setTotal={totalCountHandler} />
+      <Counter totalCount={totalCount} setTotal={totalCountHandler} />
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import '../App.css';
+import React, { useState } from 'react';
 
-function Counter() {
+function Counter({ totalCount, setTotal }) {
+  const [count, setCount] = useState(0);
+
+  const countHandler = () => {
+    setCount((prev) => prev + 1); // 이전 상태 값에 의존해서 상태 값을 변경해야 할 때는 이렇게 하는 것이 안전하다.
+    setTotal();
+  };
+
   return (
     <div className='counter'>
-      <span className='number'>0</span>
-      <button className='button'>Add +</button>
+      <span className='number'>
+        {count}
+        <span className='number__total_num'>/ {totalCount}</span>
+      </span>
+      <button className='button' onClick={countHandler}>
+        Add +
+      </button>
     </div>
   );
 }
