b573a41bb29cd2fcd18e72d501b7235694d055a9
pji0219 <pji219@naver.com>
2022-11-09 23:43:12 +0900

    state

---
+++ src/AppCounter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+function AppCounter() {
+  return (
+    <div className='container'>
+      <Counter />
+    </div>
+  );
+}
+
+export default AppCounter;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function Counter() {
+  return (
+    <div className='counter'>
+      <span className='number'>0</span>
+      <button className='button'>Add +</button>
+    </div>
+  );
+}
+
+export default Counter;
