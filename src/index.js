bd21af94258ebd3c1203a57927ea8dde1c64ce0d
pji0219 <pji219@naver.com>
2023-03-27 23:33:23 +0900

    context API
    context API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppTheme from './AppTheme';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AppTheme />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
