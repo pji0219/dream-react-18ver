509d7ad0fc7a460d231dfaa8fccb096029f1ece1
pji0219 <pji219@naver.com>
2023-03-28 23:43:03 +0900

    1

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -14,7 +14,7 @@ export const DarkModeContext = createContext();
 */
 export function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => setDarkMode((mode) => !mode);
+  const toggleDarkMode = () => setDarkMode((prev) => !prev);
 
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
