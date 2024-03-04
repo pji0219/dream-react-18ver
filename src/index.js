15bbed1c3988bb0d71601d969d7290bb2df9f255
pji0219 <pji219@naver.com>
2024-03-04 23:46:25 +0900

    basic

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppProducts from './basic2/AppProducts';
+import AppTheme from './deep2/AppTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppProducts />);
+root.render(<AppTheme />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
