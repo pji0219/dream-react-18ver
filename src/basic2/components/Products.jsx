188a03e87137626a228782f0e2103dcfb2a2eb57
pji0219 <pji219@naver.com>
2024-03-06 23:36:38 +0900

    basic

--- src/basic2/components/Products.jsx
+++ src/basic2/components/Products.jsx
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
+import useProducts from '../../deep2/hooks/use-products';
 
-export default function Products() {
-  const [conunt, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
+function Products() {
+  const [checked, setChecked] = useState(false);
+  const [isLoading, error, products] = useProducts({ salesOnly: checked });
 
-  return <div>Products</div>;
+  const handleChange = () => setChecked((prev) => !prev);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <input
+        id='checkbox'
+        type='checkbox'
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
+
+export default Products;
